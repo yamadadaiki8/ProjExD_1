181a24c4158f1c268541630529400947e4948774
daiki <c0c2508016@edu.teu.ac.jp>
2026-04-14 15:48:11 +0900

    背景画像が右から左に流れる

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #練習2
+        x = tmr # 練習5
+
+        screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
