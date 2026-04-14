ac6b9328968c5932fe37525d0246e442c44ef4bd
daiki <c0c2508016@edu.teu.ac.jp>
2026-04-14 15:32:09 +0900

    練習1：2背景画像の描画

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        # screen.blit(bg_img, [0, 0]) #練習2
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
