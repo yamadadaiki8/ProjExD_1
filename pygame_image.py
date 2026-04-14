404ee5e4b6830d86ebee03a5386b2e82847a41bd
daiki <c0c2508016@edu.teu.ac.jp>
2026-04-14 16:02:41 +0900

    練習7：8：9画像の動かし、ループの仕方

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    bg_img2 = pg.transform.flip(bg_img,True,False)#練習8
     kk_img = pg.image.load("fig/3.png")#練習3
     kk_img = pg.transform.flip(kk_img,True,False)
     tmr = 0
@@ -17,13 +18,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr # 練習5
-
+        x = tmr%3200 # 練習5
         screen.blit(bg_img, [-x, 0]) #練習2
+        screen.blit(bg_img2, [-x+1600, 0]) #練習7
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(20000)
+        
+
 
 
 if __name__ == "__main__":
