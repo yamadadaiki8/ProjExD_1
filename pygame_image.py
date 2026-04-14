d9a31ac0de5a39fdde564698b56837227c3dd35f
daiki <c0c2508016@edu.teu.ac.jp>
2026-04-14 15:41:01 +0900

    練習3：4工科トン画像の描写

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    kk_img = pg.image.load("fig/3.png")#練習3
+    kk_img = pg.transform.flip(kk_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        # screen.blit(bg_img, [0, 0]) #練習2
+        screen.blit(bg_img, [0, 0]) #練習2
+        screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
