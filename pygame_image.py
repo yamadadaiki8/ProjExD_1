bd04bd3d90c4ba7dbe71518f3f358d8d71313b0f
daiki <c0c2508016@edu.teu.ac.jp>
2026-04-14 16:20:36 +0900

    練習10

--- pygame_image.py
+++ pygame_image.py
@@ -13,19 +13,30 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)#練習8
     kk_img = pg.image.load("fig/3.png")#練習3
     kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect() #練習10　工科トンrectの取得
+    kk_rct.center = 300, 200 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed() #練習10　キー入力判定
+        if key_lst[pg.K_UP]:#練習10
+            kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1,0)
         x = tmr%3200 # 練習5
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0]) #練習7
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, [300, 200])#練習4
+        screen.blit(bg_img, [-x+3200, 0])#練習9
+        screen.blit(kk_img, kk_rct)#練習4:10
         pg.display.update()
         tmr += 1        
-        clock.tick(20000)
+        clock.tick(200)
         
 
 
